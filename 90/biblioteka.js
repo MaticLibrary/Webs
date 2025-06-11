d80a87dedb8f38d68ca77035d290522c90f5b47d
MaticLibrary <150859422+MaticLibrary@users.noreply.github.com>
2025-06-11 13:57:35 +0200

    Add files via upload

---
+++ 90/biblioteka.js
@@ -0,0 +1,5 @@
+// w plikach o rozszerzeniu JS nie stawia się  znacznikó 
+//tu może być tylko wyłączenie poprawny kod JS
+// Brak stosowania instrukcji HTML5
+document.write("To jest tekst wywołany z zewnterznego pliku JS");
+document.write("<br><br>");
